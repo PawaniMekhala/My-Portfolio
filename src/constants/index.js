241a6ddd506f4417d143e87e9cd984394b6d1d40
pawani mekhala <pawanimekhala3@gmail.com>
2024-06-14 01:03:24 +0530

    journey and about me added

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,15 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Welcome to my portfolio! I'm P.G. Pawani Mekhala, an undergraduate student 
+at Sabaragamuwa University of Sri Lanka, pursuing a B.Sc. (Hons) in Computing & Information Systems. 
+With a passion for software engineering, I am dedicated to launching innovative 
+projects and contributing to organizational success.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const MY_JOURNEY = `My journey into the world of computing began with a fascination for technology's 
+ability to solve real-world problems. As I pursued my degree, I immersed myself in diverse aspects of 
+software development, including web and mobile application development, database management, and 
+front-end and back-end programming.`;
 
 export const EXPERIENCES = [
   {
