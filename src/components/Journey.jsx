711161794a3c32958958ee333cdda362212a0696
pawani mekhala <pawanimekhala3@gmail.com>
2024-06-16 01:23:30 +0530

    framer motion added

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -1,5 +1,6 @@
 import aboutImg from "../assets/about.jpg"
 import {MY_JOURNEY} from "../constants"
+import { motion } from "framer-motion"
 
 const Journey = () => {
   return (
@@ -7,16 +8,24 @@ const Journey = () => {
       <h1 className="my-20 text-center text-4xl">My <span className="text-neutral-500">Journey</span>
       </h1>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+           whileInView={{opacity: 1, x: 0}} 
+           initial={{opacity: 0, x: -100}} 
+           transition={{duration: 0.5}} 
+           className="w-full lg:w-1/2 lg:p-8">
         <div className="flex items-center justify-center">
           <img className="rounded-2xl" src={aboutImg} alt="journey"/>
         </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div 
+           whileInView={{opacity: 1, x: 0}} 
+           initial={{opacity: 0, x: 100}}
+           transition={{duration: 0.5}} 
+           className="w-full lg:w-1/2">
         <div className="flex justify-center lg:justify-start">
           <p className="my-2 max-w-xl py-6">{MY_JOURNEY}</p>
         </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
