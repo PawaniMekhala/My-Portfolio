4b758ae370d41e1ce6d28142d88813fc3532d13d
pawani mekhala <pawanimekhala3@gmail.com>
2024-08-17 23:47:46 +0530

    changes added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -83,7 +83,7 @@ const Hero = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-light tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-10 text-2xl font-light tracking-tight lg:mt-16 lg:text-7xl"
             >Pawani Mekhala
             </motion.h1>
             <motion.span
