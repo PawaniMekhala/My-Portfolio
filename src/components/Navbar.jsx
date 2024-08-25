18df8ad61b61c6d425389bb6c1373f2133d7da54
pawani mekhala <pawanimekhala3@gmail.com>
2024-08-25 14:26:52 +0530

    modify nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import logo from '../assets/kevinRushLogo.png'
 import { FaFacebook, FaGithub, FaHackerrank, FaLinkedin } from 'react-icons/fa'
 const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-center justify-between py-6'>
-        <div className='flex flex-shrink-0 items-center'>
+    <nav className='mb-10 flex items-center justify-between py-4'>
+        {/* <div className='flex flex-shrink-0 items-center'>
             <img className='mx-3 w-12' src={logo} alt='logo' />
-        </div>
-        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        </div> */}
+        <div className='m-4 flex items-center justify-center gap-4 text-2xl'>
           <a href="https://www.linkedin.com/in/pawani-mekhala/" target="_blank" rel="pawani-mekhala">
           <FaLinkedin/></a>
           <a href="https://github.com/PawaniMekhala" target="_blank" rel="pawani-mekhala">
